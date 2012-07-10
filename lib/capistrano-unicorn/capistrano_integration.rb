2cd69e73009762561a69b17bc2c56a46d109ceeb
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2012-07-10 08:55:42 -0400

    Lazy load current_path for capistrano/multistage

--- lib/capistrano-unicorn/capistrano_integration.rb
+++ lib/capistrano-unicorn/capistrano_integration.rb
@@ -36,9 +36,9 @@ module CapistranoUnicorn
         before [ 'unicorn:start', 'unicorn:stop', 'unicorn:shutdown', 
                  'unicorn:restart', 'unicorn:reload', 'unicorn:add_worker',  
                  'unicorn:remove_worker' ] do
-          _cset(:unicorn_pid, "#{fetch(:current_path)}/tmp/pids/unicorn.pid")
-          _cset(:app_env, (fetch(:rails_env) rescue 'production'))
-          _cset(:unicorn_env, (fetch(:app_env)))
+          _cset(:unicorn_pid) { "#{fetch(:current_path)}/tmp/pids/unicorn.pid" }
+          _cset(:app_env) { (fetch(:rails_env) rescue 'production') }
+          _cset(:unicorn_env) { fetch(:app_env) }
           _cset(:unicorn_bin, "unicorn")
         end
 
